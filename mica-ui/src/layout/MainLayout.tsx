e5c9739ee46378b4eb51010ab9fd8f1ca1a12d8c
Ivan Bodrov <ibodrov@gmail.com>
2023-11-24 14:47:22 -0500

    mica-ui: remove sidebar animation

--- mica-ui/src/layout/MainLayout.tsx
+++ mica-ui/src/layout/MainLayout.tsx
@@ -26,17 +26,9 @@ const AppBar = styled(MuiAppBar, {
     shouldForwardProp: (prop) => prop !== 'open',
 })<AppBarProps>(({ theme, open }) => ({
     zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-    }),
     ...(open && {
         marginLeft: drawerWidth,
         width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
     }),
 }));
 
@@ -47,17 +39,9 @@ const Drawer = styled(MuiDrawer, {
         position: 'relative',
         whiteSpace: 'nowrap',
         width: drawerWidth,
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
         boxSizing: 'border-box',
         ...(!open && {
             overflowX: 'hidden',
-            transition: theme.transitions.create('width', {
-                easing: theme.transitions.easing.sharp,
-                duration: theme.transitions.duration.leavingScreen,
-            }),
             width: theme.spacing(7),
             [theme.breakpoints.up('sm')]: {
                 width: theme.spacing(9),
