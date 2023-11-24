aaba69cd7a3db584cde74ff7a8b392aad056a3b5
Ivan Bodrov <ibodrov@gmail.com>
2023-11-23 20:03:37 -0500

    mica-server: provide etags for ui resources

--- mica-concord-server-plugin/src/main/java/ca/ibodrov/mica/server/ui/UiServlet.java
+++ mica-concord-server-plugin/src/main/java/ca/ibodrov/mica/server/ui/UiServlet.java
@@ -1,5 +1,6 @@
 package ca.ibodrov.mica.server.ui;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.common.io.ByteStreams;
 
 import javax.servlet.annotation.WebServlet;
@@ -7,7 +8,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.WebApplicationException;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
 import java.util.Optional;
 
 import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
@@ -20,14 +24,17 @@ import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
 public class UiServlet extends HttpServlet {
 
     private static final String RESOURCE_ROOT = "META-INF/mica-ui/";
-    private static final String INDEX_HTML = RESOURCE_ROOT + "index.html";
-    private static final String TEXT_HTML_TYPE = "text/html";
+    private static final String INDEX_HTML = "index.html";
+    private static final String CHECKSUMS_FILE = "META-INF/mica-ui.checksums.cvs";
+
+    private final Map<String, UiResource> resources;
 
     public UiServlet() {
-        var cl = UiServlet.class.getClassLoader();
-        if (cl.getResource(INDEX_HTML) == null) {
-            throw new RuntimeException("Missing the root UI resource: %s. Classpath issues?".formatted(INDEX_HTML));
-        }
+        // a quick sanity check
+        resources = loadResources(CHECKSUMS_FILE);
+        Optional.ofNullable(resources.get(INDEX_HTML))
+                .orElseThrow(() -> new RuntimeException(
+                        "Missing the root UI resource: %s. Classpath issues?".formatted(RESOURCE_ROOT + INDEX_HTML)));
     }
 
     @Override
@@ -44,22 +51,26 @@ public class UiServlet extends HttpServlet {
             path = "index.html";
         }
 
-        var contentType = getContentType(path);
         try {
-            var cl = UiServlet.class.getClassLoader();
-            var in = cl.getResourceAsStream(RESOURCE_ROOT + path);
-            if (in == null) {
-                in = cl.getResourceAsStream(INDEX_HTML);
-                contentType = Optional.of(TEXT_HTML_TYPE);
-            }
-            assert in != null;
-            assert contentType.isPresent();
-            try {
-                resp.setStatus(200);
-                resp.setHeader("Content-Type", contentType.get());
-                ByteStreams.copy(in, resp.getOutputStream());
-            } finally {
-                in.close();
+            var resource = Optional.ofNullable(resources.get(path))
+                    .orElseGet(() -> resources.get(INDEX_HTML));
+
+            var filePath = RESOURCE_ROOT + resource.path();
+            try (var in = UiServlet.class.getClassLoader().getResourceAsStream(filePath)) {
+                if (in == null) {
+                    throw new RuntimeException("Resource not found: " + filePath);
+                }
+
+                resp.setHeader("Content-Type", resource.contentType());
+                resp.setHeader("ETag", resource.eTag());
+
+                var ifNoneMatch = req.getHeader("If-None-Match");
+                if (resource.eTag().equals(ifNoneMatch)) {
+                    resp.setStatus(304);
+                } else {
+                    resp.setStatus(200);
+                    ByteStreams.copy(in, resp.getOutputStream());
+                }
             }
         } catch (IOException e) {
             throw new WebApplicationException(INTERNAL_SERVER_ERROR);
@@ -81,5 +92,46 @@ public class UiServlet extends HttpServlet {
             case "woff2" -> "font/woff2";
             default -> null;
         });
+    }
+
+    private Map<String, UiResource> loadResources(String file) {
+        var resources = ImmutableMap.<String, UiResource>builder();
+
+        var cl = UiServlet.class.getClassLoader();
+        try (var in = cl.getResourceAsStream(file)) {
+            if (in == null) {
+                throw new RuntimeException(file + " file not found. Classpath or build issues?");
+            }
+
+            try (var reader = new BufferedReader(new InputStreamReader(in))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    line = line.trim();
+
+                    if (line.startsWith("#")) {
+                        continue;
+                    }
+
+                    var items = line.split(",");
+                    if (items.length != 2) {
+                        throw new RuntimeException(file + " file, invalid line: " + line);
+                    }
+
+                    var path = items[0];
+                    var eTag = items[1];
+                    var contentType = getContentType(path)
+                            .orElseThrow(() -> new RuntimeException("Can't determine Content-Type for " + path));
+
+                    resources.put(path, new UiResource(path, contentType, eTag));
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return resources.build();
+    }
+
+    private record UiResource(String path, String contentType, String eTag) {
     }
 }
