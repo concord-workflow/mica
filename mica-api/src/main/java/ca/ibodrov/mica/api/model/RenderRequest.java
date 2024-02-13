e5f208078f49f09b7480bd784c3cd6dbb1837359
Ivan Bodrov <ibodrov@gmail.com>
2024-02-13 10:43:43 -0500

    mica-api: fix view name validation

--- mica-api/src/main/java/ca/ibodrov/mica/api/model/RenderRequest.java
+++ mica-api/src/main/java/ca/ibodrov/mica/api/model/RenderRequest.java
@@ -3,12 +3,32 @@ package ca.ibodrov.mica.api.model;
 import ca.ibodrov.mica.api.validation.ValidName;
 import com.fasterxml.jackson.databind.JsonNode;
 
+import javax.validation.ConstraintViolationException;
 import java.util.Optional;
+import java.util.Set;
+
+import static java.util.Objects.requireNonNull;
 
 public record RenderRequest(Optional<EntityId> viewId,
-        Optional<@ValidName String> viewName,
+        Optional<String> viewName,
         int limit,
         Optional<JsonNode> parameters) {
+
+    public RenderRequest {
+        requireNonNull(viewId);
+        requireNonNull(viewName);
+
+        if (viewId.isEmpty() && viewName.isEmpty()) {
+            throw new ConstraintViolationException("One of viewId or viewName must be set", Set.of());
+        }
+
+        viewName.ifPresent(name -> {
+            if (!name.matches(ValidName.NAME_PATTERN)) {
+                throw new ConstraintViolationException("Invalid view name: " + name + ". " + ValidName.MESSAGE,
+                        Set.of());
+            }
+        });
+    }
 
     public static RenderRequest of(String viewName, int limit) {
         return new RenderRequest(Optional.empty(), Optional.of(viewName), limit, Optional.empty());
--- mica-api/src/main/java/ca/ibodrov/mica/api/validation/ValidName.java
+++ mica-api/src/main/java/ca/ibodrov/mica/api/validation/ValidName.java
@@ -12,15 +12,19 @@ import static java.lang.annotation.ElementType.TYPE_USE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @NotEmpty
-@Pattern(regexp = "/[a-zA-Z0-9.\\-_/\\\\]{3,1023}")
+@Pattern(regexp = ValidName.NAME_PATTERN)
 @Retention(RUNTIME)
 @Target({ FIELD, TYPE_USE })
 @Constraint(validatedBy = {})
 public @interface ValidName {
 
-    String message() default "Names should start with a forward slash '/' and can include lowercase and uppercase " +
+    String NAME_PATTERN = "/[a-zA-Z0-9.\\-_/\\\\]{3,1023}";
+
+    String MESSAGE = "Names should start with a forward slash '/' and can include lowercase and uppercase " +
             "letters (a-z, A-Z), digits, dots, dashes, and forward slashes. Length must be between 4 and 1024 " +
             "characters (including the first slash).";
+
+    String message() default MESSAGE;
 
     Class<?>[] groups() default {};
 
