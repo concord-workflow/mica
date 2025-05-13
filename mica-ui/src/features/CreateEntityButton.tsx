5516c3278b01e372728cfc533039d6e27c044bb0
Ivan Bodrov <ibodrov@gmail.com>
2025-05-13 15:34:59 -0400

    mica-ui: re-organize create entity menu

--- mica-ui/src/features/CreateEntityButton.tsx
+++ mica-ui/src/features/CreateEntityButton.tsx
@@ -1,12 +1,42 @@
-import { MICA_KIND_KIND, listEntities } from '../api/entity.ts';
+import {
+    EntityList,
+    MICA_DASHBOARD_KIND,
+    MICA_KIND_KIND,
+    MICA_VIEW_KIND,
+    listEntities,
+} from '../api/entity.ts';
 import entityKindToIcon from '../components/entityKindToIcon.tsx';
 import AddIcon from '@mui/icons-material/Add';
-import { Button, Menu, MenuItem } from '@mui/material';
+import { Button, ListSubheader, Menu, MenuItem, Tooltip } from '@mui/material';
 import ListItemIcon from '@mui/material/ListItemIcon';
 
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+
+interface KindToPick {
+    name: string;
+    label: string;
+    tooltip?: string;
+}
+
+const STANDARD_KINDS: Array<KindToPick> = [
+    {
+        name: MICA_KIND_KIND,
+        label: 'Kind',
+        tooltip: "Create new entity that can be used as a 'kind' in other entities.",
+    },
+    {
+        name: MICA_VIEW_KIND,
+        label: 'View',
+        tooltip: 'Create a new view to fetch, aggregate and filter data from other entities.',
+    },
+    {
+        name: MICA_DASHBOARD_KIND,
+        label: 'Dashboard',
+        tooltip: 'Create a new dashboard entity to visualize data rendered using views.',
+    },
+];
 
 interface Props {
     path: string;
@@ -15,10 +45,14 @@ interface Props {
 const CreateEntityButton = ({ path }: Props) => {
     const navigate = useNavigate();
 
-    const { data, isFetching } = useQuery({
+    const { data, isFetching } = useQuery<EntityList, Error, Array<KindToPick>>({
         queryKey: ['entity', 'list', '/', MICA_KIND_KIND],
         queryFn: () => listEntities({ entityKind: MICA_KIND_KIND }),
-        select: ({ data }) => data.sort((a, b) => a.name.localeCompare(b.name)),
+        select: ({ data }) =>
+            data
+                .filter((e) => STANDARD_KINDS.find((k) => k.name === e.name) === undefined)
+                .sort((a, b) => a.name.localeCompare(b.name))
+                .map(({ name }) => ({ name, label: name })),
         placeholderData: (prev) => prev,
     });
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
@@ -39,6 +73,16 @@ const CreateEntityButton = ({ path }: Props) => {
         [navigate],
     );
 
+    const KindItem = ({ row }: { row: KindToPick }) => (
+        <Tooltip title={row.tooltip}>
+            <MenuItem
+                onClick={() => handleClick(row.name, (path !== '/' ? path : '') + '/myEntity')}>
+                <ListItemIcon>{entityKindToIcon(row.name)}</ListItemIcon>
+                {row.label}
+            </MenuItem>
+        </Tooltip>
+    );
+
     return (
         <>
             <Button
@@ -49,18 +93,22 @@ const CreateEntityButton = ({ path }: Props) => {
             </Button>
             <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
                 {(isFetching || !data) && <MenuItem disabled>Loading...</MenuItem>}
-                {!isFetching &&
-                    data &&
-                    data.map((row) => (
-                        <MenuItem
-                            key={row.id}
-                            onClick={() =>
-                                handleClick(row.name, (path !== '/' ? path : '') + '/myEntity')
-                            }>
-                            <ListItemIcon>{entityKindToIcon(row.name)}</ListItemIcon>
-                            {row.name}
-                        </MenuItem>
-                    ))}
+                {!isFetching && (
+                    <>
+                        <ListSubheader>Standard entities</ListSubheader>
+                        {STANDARD_KINDS.map((row) => (
+                            <KindItem key={row.name} row={row} />
+                        ))}
+                    </>
+                )}
+                {!isFetching && data && (
+                    <>
+                        <ListSubheader>Custom kinds</ListSubheader>
+                        {data.map((row) => (
+                            <KindItem key={row.name} row={row} />
+                        ))}
+                    </>
+                )}
             </Menu>
         </>
     );
