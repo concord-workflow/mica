b8d89d2ae7d424850848c8c60c5c32b962b64659
Ivan Bodrov <ibodrov@gmail.com>
2025-05-14 13:08:28 -0400

    mica-ui: apply current color scheme to API page

--- mica-ui/src/pages/ApiPage.tsx
+++ mica-ui/src/pages/ApiPage.tsx
@@ -1,4 +1,4 @@
-import { CircularProgress } from '@mui/material';
+import { Box, CircularProgress, useColorScheme } from '@mui/material';
 
 import { Suspense, lazy } from 'react';
 import 'swagger-ui-react/swagger-ui.css';
@@ -7,10 +7,31 @@ import './ApiPage.css';
 
 const SwaggerUI = lazy(() => import('swagger-ui-react'));
 
+const useClassName = (): string | undefined => {
+    const { mode, systemMode } = useColorScheme();
+    if (mode === 'dark' || (mode === 'system' && systemMode === 'dark')) {
+        return 'dark';
+    }
+};
+
 const ApiPage = () => {
+    const className = useClassName();
+
     return (
-        <Suspense fallback={<CircularProgress />}>
-            <SwaggerUI url="/api/mica/swagger.json" />
+        <Suspense
+            fallback={
+                <Box
+                    width="100%"
+                    height="100%"
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center">
+                    <CircularProgress />
+                </Box>
+            }>
+            <span className={className}>
+                <SwaggerUI url="/api/mica/swagger.json" />
+            </span>
         </Suspense>
     );
 };
