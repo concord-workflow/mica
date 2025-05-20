d77fae425f65590cf68f6f09416a25f38756df60
Ivan Bodrov <ibodrov@gmail.com>
2025-05-20 15:11:00 -0400

    mica-server: import data as ZIP archive

--- mica-concord-server-plugin/src/main/java/ca/ibodrov/mica/server/api/ImportResource.java
+++ mica-concord-server-plugin/src/main/java/ca/ibodrov/mica/server/api/ImportResource.java
@@ -2,6 +2,9 @@ package ca.ibodrov.mica.server.api;
 
 import ca.ibodrov.mica.api.model.DataExport;
 import ca.ibodrov.mica.db.MicaDB;
+import ca.ibodrov.mica.server.exceptions.ApiException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.walmartlabs.concord.common.IOUtils;
 import com.walmartlabs.concord.db.MainDB;
 import com.walmartlabs.concord.server.sdk.metrics.WithTimer;
 import com.walmartlabs.concord.server.sdk.rest.Resource;
@@ -15,6 +18,9 @@ import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.util.Map;
 
 import static ca.ibodrov.mica.db.jooq.tables.MicaEntities.MICA_ENTITIES;
@@ -34,6 +40,7 @@ import static com.walmartlabs.concord.server.jooq.tables.Teams.TEAMS;
 import static com.walmartlabs.concord.server.jooq.tables.Users.USERS;
 import static java.util.Objects.requireNonNull;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.MediaType.APPLICATION_OCTET_STREAM;
 
 @Tag(name = "Data Import")
 @Path("/api/mica/v1/import")
@@ -43,11 +50,59 @@ public class ImportResource implements Resource {
 
     private final DSLContext concordDsl;
     private final DSLContext micaDsl;
+    private final ObjectMapper objectMapper;
 
     @Inject
-    public ImportResource(@MainDB Configuration mainCfg, @MicaDB Configuration micaCfg) {
+    public ImportResource(@MainDB Configuration mainCfg,
+                          @MicaDB Configuration micaCfg,
+                          ObjectMapper objectMapper) {
+
         this.concordDsl = requireNonNull(mainCfg).dsl();
         this.micaDsl = requireNonNull(micaCfg).dsl();
+        this.objectMapper = requireNonNull(objectMapper);
+    }
+
+    @POST
+    @WithTimer
+    @Operation(summary = "Import data as a ZIP archive", operationId = "importDataZip")
+    @Consumes(APPLICATION_OCTET_STREAM)
+    @Produces(APPLICATION_JSON)
+    public ImportResponse importData(InputStream in, @QueryParam("confirmation") String confirmation) {
+        if (!Roles.isAdmin()) {
+            throw new UnauthorizedException("Only users with the admin role can import data.");
+        }
+
+        if (!"it_might_destroy_existing_data".equals(confirmation)) {
+            throw new IllegalArgumentException(
+                    "Requires confirmation. Pass 'confirmation=it_might_destroy_existing_data' query parameter.");
+        }
+
+        java.nio.file.Path tmpDir = null;
+        try {
+            tmpDir = IOUtils.createTempDir("import");
+            IOUtils.unzip(in, tmpDir);
+
+            var exportFile = tmpDir.resolve("export.json");
+            if (!Files.exists(exportFile) || !Files.isRegularFile(exportFile)) {
+                throw ApiException.badRequest("Expected to find an export.json file in the ZIP archive.");
+            }
+
+            try (var reader = Files.newBufferedReader(exportFile)) {
+                var dataExport = objectMapper.readValue(reader, DataExport.class);
+                doImport(dataExport);
+                return new ImportResponse(true);
+            }
+        } catch (IOException e) {
+            throw ApiException.internalError("Failed to import a ZIP archive: " + e.getMessage());
+        } finally {
+            if (tmpDir != null) {
+                try {
+                    IOUtils.deleteRecursively(tmpDir);
+                } catch (IOException e) {
+                    log.warn("Failed to delete the temporary directory: {}", tmpDir);
+                }
+            }
+        }
     }
 
     @POST
@@ -65,6 +120,12 @@ public class ImportResource implements Resource {
                     "Requires confirmation. Pass 'confirmation=it_might_destroy_existing_data' query parameter.");
         }
 
+        doImport(dataExport);
+
+        return new ImportResponse(true);
+    }
+
+    private void doImport(DataExport dataExport) {
         concordDsl.transaction(cfg -> {
             var tx = cfg.dsl();
 
@@ -119,8 +180,6 @@ public class ImportResource implements Resource {
             dataExport.micaEntityHistory()
                     .forEach(r -> upsert(tx, MICA_ENTITY_HISTORY, r));
         });
-
-        return new ImportResponse(true);
     }
 
     public record ImportResponse(boolean ok) {
