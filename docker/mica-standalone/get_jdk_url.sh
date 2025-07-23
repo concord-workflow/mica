3004bd890caa3baf5eeddfe29422e069472c8fbb
Ivan Bodrov <ibodrov@gmail.com>
2025-07-23 12:29:10 -0400

    docker: update JDK

--- docker/mica-standalone/get_jdk_url.sh
+++ docker/mica-standalone/get_jdk_url.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-JDK_21_AMD64="https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.7%2B6/OpenJDK21U-jdk_x64_linux_hotspot_21.0.7_6.tar.gz"
-JDK_21_ARM64="https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.7%2B6/OpenJDK21U-jdk_aarch64_linux_hotspot_21.0.7_6.tar.gz"
+JDK_21_AMD64="https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.7%2B6/OpenJDK21U-jdk_x64_linux_hotspot_21.0.8_9.tar.gz"
+JDK_21_ARM64="https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.7%2B6/OpenJDK21U-jdk_aarch64_linux_hotspot_21.0.8_9.tar.gz"
 
 case "${JDK_VERSION}-${TARGETARCH}" in
   "21-amd64")
